f0b64164012f1ada6392c98f66db2f43f7df0736
Oleg Fateev <fateev010405@gmail.com>
2025-07-25 00:31:42 +0300

    Реализована динамика компонента TodoItem при изменении полей ввода чекбокс и текст с задачей

--- src/components/NewTodoItem.jsx
+++ src/components/NewTodoItem.jsx
@@ -1,29 +1,29 @@
 import { useState } from "react"
 
-export const NewTodoItem = ({setAddedNewTodoItemFlag}) => {
+export const NewTodoItem = ({ setAddedNewTodoItemFlag }) => {
     const [newTitle, setNewTitle] = useState("");
 
     const isValid = newTitle.trim().length > 0;
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        fetch('http://localhost:3000/todos', {
+        await fetch('http://localhost:3000/todos', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json;charset=utf-8' },
             body: JSON.stringify({
                 title: newTitle,
                 completed: false,
             })
-        }).finally(() => {
-            setAddedNewTodoItemFlag(true);
-            setNewTitle("");
         });
+        
+        setAddedNewTodoItemFlag(true);
+        setNewTitle("");
     }
 
     return (
         <li>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Введите задачу" value={newTitle} onChange={({target}) => setNewTitle(target.value)} />
+                <input type="text" placeholder="Введите задачу" value={newTitle} onChange={({ target }) => setNewTitle(target.value)} />
                 <button type="submit" disabled={!isValid}>Сохранить</button>
                 {isValid ? <button type="button" onClick={() => setNewTitle("")}>Сбросить</button> : ""}
             </form>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,20 @@
-export const TodoItem = ({id, title, completed}) => {
+import { useState } from 'react';
+
+export const TodoItem = ({ id, title, completed }) => {
+    const [itemContent, setItemContent] = useState({ id, title, completed });
+    const isChanged = (itemContent.completed !== completed) || (itemContent.title !== title) ? true : false;
+
+    const handleSubmit = (event) => { event.preventDefault() }
+    const handleOnChangeCheckbox = ({ target }) => setItemContent({ ...itemContent, completed: target.checked });
+    const handleOnChangeInput = ({target}) => { setItemContent({...itemContent, title: target.value}) };
+
     return (
         <li key={id}>
-            <form>
-                <input type="checkbox" checked={completed} />
-                <input type="text" value={title}/>
-                <button>Удалить</button>
+            <form onSubmit={handleSubmit}>
+                <input type="checkbox" checked={itemContent.completed} onChange={handleOnChangeCheckbox} />
+                <input type="text" value={itemContent.title} onChange={handleOnChangeInput} />
+                {isChanged ? <button type="submit">Сохранить</button> : ""}
+                <button type="button">Удалить</button>
             </form>
         </li>
     )
