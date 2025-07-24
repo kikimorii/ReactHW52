b05931e1ab07823ee2d3f6d8b274062495934a16
Oleg Fateev <fateev010405@gmail.com>
2025-07-25 01:09:19 +0300

    Реализован функционал изменения данных в списке TodoList'а

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react';
+import styles from "./TodoItem.module.css";
 
 export const TodoItem = ({ id, title, completed, setIsChangeList }) => {
     const [itemContent, setItemContent] = useState({ id, title, completed });
     const isChanged = (itemContent.completed !== completed) || (itemContent.title !== title) ? true : false;
 
-    const handleSubmit = (event) => { event.preventDefault() }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (confirm(`Вы точно хотите изменить задачу?\n"${title}"`)) {
+            fetch(`http://localhost:3000/todos/${id}`, {
+                method: "PUT",
+                headers: { 'Content-Type': 'application/json;charset=utf-8' },
+                body: JSON.stringify({
+                    title: itemContent.title,
+                    completed: itemContent.completed,
+                })
+            }).finally(() => setIsChangeList(true))
+        }
+    }
     const handleOnChangeCheckbox = ({ target }) => setItemContent({ ...itemContent, completed: target.checked });
     const handleOnChangeInput = ({ target }) => { setItemContent({ ...itemContent, title: target.value }) };
 
@@ -19,9 +32,21 @@ export const TodoItem = ({ id, title, completed, setIsChangeList }) => {
     return (
         <li key={id}>
             <form onSubmit={handleSubmit}>
-                <input type="checkbox" checked={itemContent.completed} onChange={handleOnChangeCheckbox} />
-                <input type="text" value={itemContent.title} onChange={handleOnChangeInput} />
-                {isChanged ? <button type="submit">Сохранить</button> : ""}
+                <input
+                    type="checkbox"
+                    checked={itemContent.completed}
+                    onChange={handleOnChangeCheckbox}
+                />
+                <input
+                    className={itemContent.completed ? styles.compeletedTitle : ""}
+                    type="text"
+                    value={itemContent.title}
+                    onChange={handleOnChangeInput}
+                />
+                {isChanged
+                    ? <button type="submit">Сохранить</button>
+                    : ""
+                }
                 <button onClick={handleClickToDelete} type="button">Удалить</button>
             </form>
         </li>
