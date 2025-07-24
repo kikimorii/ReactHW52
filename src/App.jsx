6ecdcbe2365984157f0c862d27b7f0cf2d5ff829
Oleg Fateev <fateev010405@gmail.com>
2025-07-25 00:01:57 +0300

    Добавлен функционал добавления нового элемента TodoList'а, а также автоматической перезагрузки TodoList компонента

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import { TodoList } from './components/TodoList';
 
 export const App = () => {
   const [todoList, setTodoList] = useState([]);
+  const [addedNewTodoItemFlag, setAddedNewTodoItemFlag] = useState(false);
 
   useEffect(() => {
     fetch('http://localhost:3000/todos')
       .then((response) => response.json())
       .then((todoListResponse) => setTodoList(todoListResponse))
-  }, []);
+      .finally(() => setAddedNewTodoItemFlag(false))
+  }, [addedNewTodoItemFlag]);
 
   
 
   return (
     <div className={styles.app}>
-      <NewTodoItem />
+      <NewTodoItem setAddedNewTodoItemFlag={setAddedNewTodoItemFlag} />
       <TodoList todoList={todoList} />
     </div>
   )
--- src/components/NewTodoItem.jsx
+++ src/components/NewTodoItem.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export const NewTodoItem = () => {
+export const NewTodoItem = ({setAddedNewTodoItemFlag}) => {
     const [newTodoItemTitle, setNewTodoItemTitle] = useState('');
     const [isValid, setIsValid] = useState(false);
     const [isEmpty, setIsEmpty] = useState(true);
@@ -16,12 +16,31 @@ export const NewTodoItem = () => {
         setNewTodoItemTitle(target.value);
     }
 
+    const handleClickSave = (event) => {
+        event.preventDefault();
+        fetch('http://localhost:3000/todos', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json;charset=utf-8' },
+            body: JSON.stringify({
+                title: newTodoItemTitle,
+                completed: false,
+            })
+        }).finally(() => setAddedNewTodoItemFlag(true));
+    }
+
+    const handleClickReset = (event) => {
+        event.preventDefault();
+        setNewTodoItemTitle("");
+        setIsValid(false);
+        setIsEmpty(true);
+    }
+
     return (
         <li>
             <form>
                 <input type="text" placeholder="Введите задачу" value={newTodoItemTitle} onChange={onChange} />
-                <button disabled={!isValid}>Сохранить</button>
-                {!isEmpty ? <button>Сбросить</button> : ""}
+                <button disabled={!isValid} onClick={handleClickSave}>Сохранить</button>
+                {!isEmpty ? <button onClick={handleClickReset}>Сбросить</button> : ""}
             </form>
         </li>
     )
