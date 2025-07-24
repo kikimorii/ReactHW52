5e1f9ac0beabf69774f11f4993d8eae169f41b0c
Oleg Fateev <fateev010405@gmail.com>
2025-07-24 23:02:21 +0300

    Добавлен компонент для добавления новых задач в TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import { NewTodoItem } from './components/NewTodoItem';
 import styles from './App.module.css';
 
 export const App = () => {
   return (
     <div className={styles.app}>
-      
+      <NewTodoItem />
     </div>
   )
 }
---
+++ src/components/NewTodoItem.jsx
@@ -0,0 +1,11 @@
+export const NewTodoItem = () => {
+    return (
+        <li>
+            <form>
+                <input type="text" placeholder="Введите задачу" />
+                <button>Сохранить</button>
+                <button>Сбросить</button>
+            </form>
+        </li>
+    )
+}
